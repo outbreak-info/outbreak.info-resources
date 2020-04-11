fe930415ff78bb858e33a3852a91b4de7ceb5940
Ben <ben@ben-rush.net>
2020-04-11 06:13:37 -0500

    cardinality and urls cleansing

--- yaml/schema.py
+++ yaml/schema.py
@@ -8,16 +8,32 @@ class Schema:
         self.id = id
 
     def add_to_context(self, name, url):
+        # issue 30. 
+        if not url.endswith("/"):
+            url = url + "/"
         self.context[name] = url
 
     def add_to_props(self, prop):
         self.graph.append(prop)
 
+    def clean_cardinality(self, result):
+        for dict in result["@graph"]: 
+            if "owl:cardinality" in dict:
+                cardinality = dict["owl:cardinality"]
+                if "one" in cardinality.lower():
+                    cardinality = "one"
+                else:
+                    cardinality = "many"
+
     def render(self, out):
-        
         result = {}
         result["@context"] = self.context
         result["@id"] = self.id
         result["@graph"] = self.graph
+
+        # do cleansing
+        self.clean_cardinality(result)
+
+        # write out
         with open(out, 'w') as fout: 
             fout.write(yaml.dump(result))
