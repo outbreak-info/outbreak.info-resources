128dc313cb03d66c361c370c012fff7e069eb930
Ben <ben@ben-rush.net>
2020-04-16 07:23:43 -0500

    classes

--- yaml/protocols.py
+++ yaml/protocols.py
@@ -1,4 +1,4 @@
-from schema import Schema
+from schema import Schema, ClassProperty
 import os
 import csv
 
@@ -10,6 +10,54 @@ schema.add_to_context("owl", "http://www.w3.org/2002/07/owl")
 schema.add_to_context("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns")
 schema.add_to_context("rdfs", "http://www.w3.org/2000/01/rdf-schema")
 schema.add_to_context("outbreak", "http://outbreak.info/")
+
+class_dir = "yaml/classes"
+class_files = [os.path.join(os.getcwd(), "yaml/classes", f) 
+    for f in os.listdir(class_dir) if os.path.isfile(os.path.join(class_dir, f))]
+
+for class_file in class_files:
+    with open(class_file, "r") as fin:
+        csv_reader = csv.reader(fin)
+        cols = []
+        class_props = []
+        for row in csv_reader:
+            if len(cols) == 0:
+                cols = row
+            else:   
+                class_prop = ClassProperty()
+                class_prop.id = row[cols.index("Property")]
+                class_prop.comment = row[cols.index("Description")]
+                class_prop.cardinality = row[cols.index("cardinality")]
+                class_prop.marginality = row[cols.index("marginality")]
+                class_prop.domain_includes = row[cols.index("domainIncludes")]
+
+                rangeIncludes = row[cols.index("expected type")]
+                rangeIncludes = [f.strip() for f in rangeIncludes.replace('[', '').replace(']','').split(',')]
+
+                if len(rangeIncludes) == 1:
+                    class_prop.rangeIncludes = {"@id" : rangeIncludes[0]}
+                else:
+                    class_prop.rangeIncludes = [{"@id" : f} for f in rangeIncludes]
+
+                sameAs = row[cols.index("sameAs")]
+                sameAs = [f.strip() for f in sameAs.replace('[', '').replace(']','').split(',')]
+
+                if len(sameAs) == 1:
+                    class_prop.sameAs = {"@id" : sameAs[0]}
+                else:
+                    class_prop.sameAs = [{"@id" : f} for f in sameAs]
+
+                class_prop.cardinality = row[cols.index("cardinality")]
+                class_prop.marginality = row[cols.index("marginality")]
+
+                class_props.append(class_prop)
+
+        file_name = class_file[class_file.rfind("\\")+1:]
+        sub_classes = [s.strip() for s in file_name[file_name.find('(')+1:len(file_name)-5].split(',')]
+        class_id = file_name[:file_name.find('(')]
+
+        schema.add_class("outbreak:" + class_id, "", class_id, sub_classes, "https://outbreak.org", class_props)
+
 
 with open("c:/users/ben/desktop/schemas/protocol.csv", "r") as fin:
     csv_reader = csv.reader(fin)
@@ -31,7 +79,6 @@ with open("c:/users/ben/desktop/schemas/protocol.csv", "r") as fin:
                 prop["rdfs:subClassOf"] = {"@id" : subClassOf[0]}
             else:
                 prop["rdfs:subClassOf"] = [{"@id" : f} for f in subClassOf]
-
 
             prop["rdfs:comment"] = row[cols.index("Description")]
             prop["owl:cardinality"] = row[cols.index("cardinality")]
--- yaml/schema.py
+++ yaml/schema.py
@@ -2,10 +2,38 @@ import yaml
 import os
 import re
 
+class ClassProperty:
+    def __init__(self):
+        self.id = ""
+        self.type = ""
+        self.comment = ""
+        self.label = ""
+        self.domain_includes = ""
+        self.range_includes = []
+        self.same_as = {}
+        self.cardinality = ""
+        self.marginality = ""
+
+    def to_dictionary(self):
+        ret = {}
+        ret["@id"] = self.id
+        ret["@type"] = self.type
+        ret["rdfs:comment"] = self.comment
+        ret["rdfs:label"] = self.label
+        ret["schema:domainIncludes"] = {"@id" : self.domain_includes}
+        ret["schema:rangeIncludes"] = self.range_includes
+        ret["schema:sameAs"] = self.same_as
+        ret["owl:cardinality"] = self.cardinality
+        ret["marginality"] = self.marginality
+
+        return ret
+
 class Schema:
     def __init__(self, id):
         self.context = {}
         self.graph = []
+        self.referenced_classes = []
+        self.referenced_class_props = []
         self.id = id
 
     def add_to_context(self, name, url):
@@ -16,6 +44,25 @@ class Schema:
 
     def add_to_props(self, prop):
         self.graph.append(prop)
+
+    def add_class(self, id, comment, label, sub_class, is_part, class_props):
+        referenced_class = {}
+        referenced_class["@id"] = id
+        referenced_class["rdfs:comment"] = comment
+        referenced_class["rdfs:label"] = label
+        referenced_class["@type"] = "rdfs:Class"
+
+        if len(sub_class) == 1:
+            referenced_class["rdfs:subClassOf"] = {"@id" : sub_class[0]}
+        else:
+            referenced_class["rdfs:subClassOf"] = [{"@id" : f} for f in sub_class]
+
+        referenced_class["rdfs:isPartOf"] = {"@id" : is_part}
+
+        self.referenced_classes.append(referenced_class)
+
+        for class_prop in class_props:
+            self.referenced_class_props.append(class_prop.to_dictionary())
 
     def clean_cardinality(self, result):
         for dict in result["@graph"]: 
@@ -45,6 +92,18 @@ class Schema:
         result = {}
         result["@context"] = self.context
         result["@id"] = self.id
+
+        temp_graph = []
+
+        # do this here so they show up first
+        for referenced_class in self.referenced_classes:
+            temp_graph.append(referenced_class)
+
+        for referenced_class_prop in self.referenced_class_props:
+            temp_graph.append(referenced_class_prop)
+
+        self.graph = temp_graph + self.graph
+
         result["@graph"] = self.graph
 
         # do cleansing
